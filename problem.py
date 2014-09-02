c3a7d104c888c685dd783a8ec516dc970ba6bb4d
Chaitanya Talnikar <talnikar@mit.edu>
2014-09-01 21:14:01 -0400

    mon night

--- problem.py
+++ problem.py
@@ -8,26 +8,26 @@ from pyRCF import Solver
 from utils import ad
 from field import CellField
 
-nSteps = 2000
-writeInterval = 20
+nSteps = 1000
+writeInterval = 10
 
-#primal = Solver('tests/convection/', {'R': 8.314, 'Cp': 1006., 'gamma': 1.4, 'mu': 0., 'Pr': 0.7, 'CFL': 0.2})
-#
-#def objective(fields):
-#    rho, rhoU, rhoE = fields
-#    mesh = rho.mesh
-#    mid = np.array([0.75, 0.5, 0.5])
-#    indices = range(0, mesh.nInternalCells)
-#    G = np.exp(-100*np.linalg.norm(mid-mesh.cellCentres[indices], axis=1)**2).reshape(-1,1)*mesh.volumes[indices]
-#    return ad.sum(rho.field[indices]*G)
-#
-#def perturb(fields):
-#    rho, rhoU, rhoE = fields
-#    mesh = rho.mesh
-#    mid = np.array([0.25, 0.5, 0.5])
-#    indices = range(0, mesh.nInternalCells)
-#    G = 1e-4*np.exp(-100*np.linalg.norm(mid-mesh.cellCentres[indices], axis=1)**2).reshape(-1,1)
-#    rho.field[indices] += G
+primal = Solver('tests/convection/', {'R': 8.314, 'Cp': 1006., 'gamma': 1.4, 'mu': 0., 'Pr': 0.7, 'CFL': 0.2})
+
+def objective(fields):
+    rho, rhoU, rhoE = fields
+    mesh = rho.mesh
+    mid = np.array([0.75, 0.5, 0.5])
+    indices = range(0, mesh.nInternalCells)
+    G = np.exp(-100*np.linalg.norm(mid-mesh.cellCentres[indices], axis=1)**2).reshape(-1,1)*mesh.volumes[indices]
+    return ad.sum(rho.field[indices]*G)/(nSteps + 1)
+
+def perturb(fields):
+    rho, rhoU, rhoE = fields
+    mesh = rho.mesh
+    mid = np.array([0.25, 0.5, 0.5])
+    indices = range(0, mesh.nInternalCells)
+    G = 1e-8*np.exp(-100*np.linalg.norm(mid-mesh.cellCentres[indices], axis=1)**2).reshape(-1,1)
+    rho.field[indices] += G
 
 #primal = Solver('tests/forwardStep/', {'R': 8.314, 'Cp': 2.5, 'gamma': 1.4, 'mu': 0., 'Pr': 0.7, 'CFL': 0.2})
 #
@@ -50,31 +50,31 @@ writeInterval = 20
 #
 #
 #
-primal = Solver('tests/cylinder/', {'R': 8.314, 'Cp': 1006, 'gamma': 1.4, 'mu': 2.5e-5, 'Pr': 0.7, 'CFL': 0.2})
+#primal = Solver('tests/cylinder/', {'R': 8.314, 'Cp': 1006, 'gamma': 1.4, 'mu': 2.5e-5, 'Pr': 0.7, 'CFL': 0.2})
+#
+#def objective(fields):
+#    rho, rhoU, rhoE = fields
+#    mesh = rhoE.mesh
+#    patchID = 'cylinder'
+#    patch = rhoE.BC[patchID]
+#    start, end = patch.startFace, patch.endFace
+#    areas = mesh.areas[start:end]
+#    nx = mesh.normals[start:end, 0]
+#    start, end = patch.cellStartFace, patch.cellEndFace
+#    p = rhoE.field[start:end]*(primal.gamma-1)
+#    deltas = np.linalg.norm(mesh.cellCentres[start:end]-mesh.cellCentres[patch.internalIndices], axis=1).reshape(-1,1)
+#    mungUx = primal.mu*(rhoU.field[start:end, 0]/rho.field[start:end]-rhoU.field[patch.internalIndices, 0]/rho.field[patch.internalIndices])/deltas
+#    return ad.sum((p*nx-mungUx)*areas)/(nSteps + 1)
+#
+#def perturb(fields):
+#    rho, rhoU, rhoE = fields
+#    patch = 'inlet'
+#    bc = rhoU.BC
+#    start, end = bc[patch].cellStartFace, bc[patch].cellEndFace
+#    rhoU.field[start:end][:,0] += 0.1
 
-def objective(fields):
-    rho, rhoU, rhoE = fields
-    mesh = rhoE.mesh
-    patchID = 'cylinder'
-    patch = rhoE.BC[patchID]
-    start, end = patch.startFace, patch.endFace
-    areas = mesh.areas[start:end]
-    nx = mesh.normals[start:end, 0]
-    start, end = patch.cellStartFace, patch.cellEndFace
-    p = rhoE.field[start:end]*(primal.gamma-1)
-    deltas = np.linalg.norm(mesh.cellCentres[start:end]-mesh.cellCentres[patch.internalIndices], axis=1).reshape(-1,1)
-    mungUx = primal.mu*(rhoU.field[start:end, 0]/rho.field[start:end]-rhoU.field[patch.internalIndices, 0]/rho.field[patch.internalIndices])/deltas
-    return ad.sum((p*nx-mungUx)*areas)/(nSteps + 1)
-
-def perturb(fields):
-    rho, rhoU, rhoE = fields
-    patch = 'inlet'
-    bc = rhoU.BC
-    start, end = bc[patch].cellStartFace, bc[patch].cellEndFace
-    rhoU.field[start:end][:,0] += 0.1
-
-startTime = 2
-dt = 1e-8
+startTime = 0
+dt = 1e-2
 
 if __name__ == "__main__":
     mesh = primal.mesh
--- pyRCF.py
+++ pyRCF.py
@@ -139,9 +139,14 @@ class Solver(object):
         UF = 0.5*(ULF + URF)
         sigmaF = self.mu*(snGrad(U) + interpolate(grad(UF, ghost=True).transpose()).dotN() - (2./3)*interpolate(div(UnF, ghost=True))*mesh.Normals)
         
-        return [ddt(rho, rho.old, self.dt) + div(rhoFlux),
-                ddt(rhoU, rhoU.old, self.dt) + div(rhoUFlux) + grad(pF) - div(sigmaF),
-                ddt(rhoE, rhoE.old, self.dt) + div(rhoEFlux) - (laplacian(e, self.alpha) + div(sigmaF.dot(UF)))]
+        #return [ddt(rho, rho.old, self.dt) + div(rhoFlux),
+        #        ddt(rhoU, rhoU.old, self.dt) + div(rhoUFlux) + grad(pF) - div(sigmaF),
+        #        ddt(rhoE, rhoE.old, self.dt) + div(rhoEFlux) - (laplacian(e, self.alpha) + div(sigmaF.dot(UF)))]
+        return [div(rhoFlux),
+                div(rhoUFlux) + grad(pF) - div(sigmaF),
+                div(rhoEFlux) - (laplacian(e, self.alpha) + div(sigmaF.dot(UF)))]
+
+
 
     def boundary(self, rhoI, rhoUI, rhoEI):
         logger.info('correcting boundary')
