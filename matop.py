878669503f722956edb69e64db11fee036fa8b2c
Chaitanya Talnikar <talnikar@mit.edu>
2014-09-16 13:23:10 -0400

    progress on matop

--- matop.py
+++ matop.py
@@ -3,36 +3,37 @@ import numpy as np
 import scipy.sparse as sp
 
 class Matrix(object):
-    def __init__(A, b=None):
+    def __init__(self, A, b=None):
         self.A = A
         m, n = A.shape
         assert m == n
+        self.b = b
         if b is None:
             self.b = np.zeros(m)
 
-    def __add__(b):
+    def __add__(self, b):
         if isinstance(b, Matrix):
             return self(self.A + b.A, self.b - b)
         else:
             return self(self.A, self.b - b)
 
-    def __sub__(b):
+    def __sub__(self, b):
         return self.__add__(-b)
 
     def __neg__(self):
         return self(-A, -b)
     
-    def __rsub__(b):
+    def __rsub__(self, b):
         pass
 
-    def __radd__(b):
-        return self.__add__(b)
+    def __radd__(self, b):
+        return self.__add__(self, b)
 
-    def __mul__(b):
+    def __mul__(self, b):
         return self(self.A * b, self.b * b)
 
-    def __rmul__(b):
-        return self.__mul__(b)
+    def __rmul__(self, b):
+        return self.__mul__(self, b)
 
     def solve(self):
         return sp.linalg.solve(self.A, self.b)  
