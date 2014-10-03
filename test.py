831c5cd19acb9688adb3bf4b7281ea4b5d0f41f6
Chaitanya Talnikar <talnikar@mit.edu>
2014-10-02 23:51:53 -0400

    added more tests

--- test.py
+++ test.py
@@ -57,4 +57,64 @@ class TestField(unittest.TestCase):
         ref = self.data['trace']
         self.assertAlmostEqual(0, np.abs(res-ref).max())
         
+class TestInterp(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.case = 'tests/cylinder/'
+        self.mesh = Mesh(self.case)
+        self.rho = CellField.read('rho', self.mesh, 2.0)
+        self.rhoU = CellField.read('rhoU', self.mesh, 2.0)
+        self.rhoE = CellField.read('rhoE', self.mesh, 2.0)
+        self.data = np.load(self.case + 'test_data.npz')
+ 
+    def test_TVD_scalar(self):
+        res = ad.value(TVD_dual(self.rho)[0].field)
+        ref = self.data['TVD_scalar']
+        self.assertAlmostEqual(0, np.abs(res-ref).max())
+
+    def test_TVD_vector(self):
+        res = ad.value(TVD_dual(self.rhoU)[1].field)
+        ref = self.data['TVD_vector']
+        self.assertAlmostEqual(0, np.abs(res-ref).max())
+
+    def test_interpolate(self):
+        res = ad.value(interpolate(self.rhoE).field)
+        ref = self.data['interpolate']
+        self.assertAlmostEqual(0, np.abs(res-ref).max())
+     
+class TestOp(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.case = 'tests/cylinder/'
+        self.mesh = Mesh(self.case)
+        self.rho = CellField.read('rho', self.mesh, 2.0)
+        self.rhoU = CellField.read('rhoU', self.mesh, 2.0)
+        self.rhoE = CellField.read('rhoE', self.mesh, 2.0)
+        self.data = np.load(self.case + 'test_data.npz')
+ 
+    def test_grad_scalar(self):
+        res = ad.value(grad(self.rho).field)
+        ref = self.data['grad_scalar']
+        self.assertAlmostEqual(0, np.abs(res-ref).max())
+
+    def test_grad_vector(self):
+        res = ad.value(grad(self.rhoU).field)
+        ref = self.data['grad_vector']
+        self.assertAlmostEqual(0, np.abs(res-ref).max())
+
+    def test_div(self):
+        res = ad.value(div(self.rhoU).field)
+        ref = self.data['div']
+        self.assertAlmostEqual(0, np.abs(res-ref).max())
+
+    def test_laplacian(self):
+        res = ad.value(laplacian(self.rhoU/self.rho, 2.5e-5).field)
+        ref = self.data['laplacian']
+        self.assertAlmostEqual(0, np.abs(res-ref).max())
+
+
+
+
+
+
         
