3331f472a56f51ab1c67540ec67f26dfb8b5e244
talnikar <talnikar@mit.edu>
2015-05-11 22:42:27 -0400

    added comments in pyRCF

--- pyRCF.py
+++ pyRCF.py
@@ -82,7 +82,7 @@ class RCF(Solver):
         mesh = self.mesh
         paddedMesh = mesh.paddedMesh
 
-        # in interpolation phi is full with additional second layer, gradField is full
+        # gradient evaluated using gauss integration rule
         gradRho = grad(central(rhoP, paddedMesh), ghost=True)
         gradRhoU = grad(central(rhoUP, paddedMesh), ghost=True)
         gradRhoE = grad(central(rhoEP, paddedMesh), ghost=True)
@@ -95,23 +95,13 @@ class RCF(Solver):
         rhoE = CellField.getOrigField(rhoEP)
         U = CellField.getOrigField(UP)
         T = CellField.getOrigField(TP)
+
         #self.local = (rhoU.field[mesh.owner[mesh.nInternalFaces + mesh.nLocalCells - mesh.nInternalCells:mesh.nFaces]])
         #self.remote = (rhoU.field[mesh.neighbour[mesh.nInternalFaces + mesh.nLocalCells - mesh.nInternalCells:mesh.nFaces]])
         self.local = (gradRho.field[mesh.owner[mesh.nInternalFaces + mesh.nLocalCells - mesh.nInternalCells:mesh.nFaces]])
         self.remote = (gradRho.field[mesh.neighbour[mesh.nInternalFaces + mesh.nLocalCells - mesh.nInternalCells:mesh.nFaces]])
 
-        #field = rhoU
-        #fieldP = rhoUP
-        #phi = ad.concatenate((field.field[mesh.owner], field.field[mesh.neighbour], mesh.weights, mesh.normals, central(field, mesh).field), axis=1)
-        #phiP = ad.concatenate((fieldP.field[paddedMesh.owner], fieldP.field[paddedMesh.neighbour], paddedMesh.weights, paddedMesh.normals, central(fieldP, paddedMesh).field), axis=1)
-        ##self.remote = phiP[mesh.nInternalFaces + mesh.nCells - mesh.nLocalCells:paddedMesh.nInternalFaces]
-        #self.remote = phiP[paddedMesh.nInternalFaces + mesh.nLocalCells - mesh.nInternalCells:paddedMesh.nFaces]
-        #self.loc = 'boundary'
-        #self.local = phi[paddedMesh.localRemoteFaces[self.loc][mesh.remotePatches[0]]]
-        #for patchID in mesh.remotePatches[1:]:
-        #    self.local = ad.concatenate((self.local, phi[paddedMesh.localRemoteFaces[self.loc][patchID]]), axis=0)
-
-        # interpolation
+        # face reconstruction
         rhoLF, rhoRF = TVD_dual(rho, gradRho)
         rhoULF, rhoURF = TVD_dual(rhoU, gradRhoU)
         rhoELF, rhoERF = TVD_dual(rhoE, gradRhoE)
@@ -136,37 +126,35 @@ class RCF(Solver):
         aF = Field.max(apF.abs(), amF.abs())
         aF.name = 'aF'
 
-        #self.local = cLF.field
-
         # CFL based time step: sparse update?
         aF2 = Field.max((UnLF + aF).abs(), (UnRF - aF).abs())*0.5
         self.dtc = self.CFL/ad.max(aF2.field/mesh.deltas)
 
-        # flux reconstruction
+        # kurganov tadmor euler flux 
         # phi (flux) for pressureInletVelocity
         rhoFlux = 0.5*(rhoLF*UnLF + rhoRF*UnRF) - 0.5*aF*(rhoRF-rhoLF)
         self.flux = 2.*rhoFlux/(rhoLF + rhoRF)
         rhoUFlux = 0.5*(rhoULF*UnLF + rhoURF*UnRF) - 0.5*aF*(rhoURF-rhoULF)
         rhoEFlux = 0.5*((rhoELF + pLF)*UnLF + (rhoERF + pRF)*UnRF) - 0.5*aF*(rhoERF-rhoELF)
-        pF = 0.5*(pLF + pRF)
-        
-        UF = 0.5*(ULF + URF)
+
         #gradUTF = interpolate(grad(UF, ghost=True))
         rhoR = Field(rho.name, rho.field.reshape((mesh.nCells, 1, 1)), (1, 1))
         gradUT = (rhoR*gradRhoU.transpose()-gradRho.outer(rhoU))/(rhoR*rhoR)
+        # TODO: change reconstruction of gradient on face
         gradUTF = central(gradUT, rho.mesh)
 
         # viscous part
+        pF = 0.5*(pLF + pRF)
+        UF = 0.5*(ULF + URF)
         TF = 0.5*(TLF + TRF)
         mu = self.mu(TF)
         kappa = self.kappa(mu, TF)
         UnF = 0.5*(UnLF + UnRF)
         sigmaF = (snGrad(U) + gradUTF.dotN() - (2./3)*mesh.Normals*gradUTF.trace())*mu
+        # TODO: check laplacian and viscous terms
 
         # source terms
         source = self.source(self)
-        #import pdb; pdb.set_trace()
-        #print(rhoFlux.field.broadcastable, div(rhoFlux).field.broadcastable)
         
         return [ddt(rho, self.dt) + div(rhoFlux) - source[0],
                 ddt(rhoU, self.dt) + div(rhoUFlux) + grad(pF) - div(sigmaF) - source[1],
