e10da73db809fac0f76802cb070f06997d539739
Chaitanya Talnikar <talnikar@mit.edu>
2015-11-16 00:15:16 -0500

    Fixed eigen.py

--- eigen.py
+++ eigen.py
@@ -17,19 +17,16 @@ import time
 import sys
 
 nEigen = 1
-
 primal.adjoint = True
 mesh = primal.mesh
-firstCheckpoint = 0
-result = 0.
-if parallel.rank == 0:
-    timeSteps = np.loadtxt(primal.timeStepFile, ndmin=2)
-    timeSteps = np.concatenate((timeSteps, np.array([[np.sum(timeSteps[-1]).round(9), 0]])))
-else:
-    timeSteps = np.zeros((nSteps + 1, 2))
-parallel.mpi.Bcast(timeSteps, root=0)
-
-t, dt = timeSteps[nSteps-writeInterval]
+#if parallel.rank == 0:
+#    timeSteps = np.loadtxt(primal.timeStepFile, ndmin=2)
+#    timeSteps = np.concatenate((timeSteps, np.array([[np.sum(timeSteps[-1]).round(9), 0]])))
+#else:
+#    timeSteps = np.zeros((nSteps + 1, 2))
+#parallel.mpi.Bcast(timeSteps, root=0)
+#t, dt = timeSteps[nSteps-writeInterval]
+t, dt = 1.120001033,2.279e-3
 fields = primal.initFields(t)
 stackedFields = primal.stackFields(fields, np)
 primal.compile()
@@ -50,6 +47,13 @@ def writeAdjointFields(stackedAdjointFields, names, writeTime):
     pprint('Time for writing fields: {0}'.format(end-start))
     pprint()
 
+
+def adjoint(stackedAdjointFields):
+    stackedAdjointFields = parallel.scatterCells(stackedAdjointFields, mesh.origMesh)
+    gradient = primal.gradient(stackedFields, stackedAdjointFields, dt)[0]
+    gradient = parallel.gatherCells(gradient, mesh.origMesh)
+    return gradient
+
 totalCells = parallel.sum(nCells)
 N = totalCells*nDims
 def operator(vector):
@@ -58,18 +62,24 @@ def operator(vector):
     operator.count += 1
     pprint(operator.count, 'operation')
     stackedAdjointFields = vector.reshape((totalCells, nDims))
-    stackedAdjointFields = parallel.scatterCells(stackedAdjointFields, mesh.origMesh)
-    gradient = primal.gradient(stackedFields, stackedAdjointFields, dt)[0]
-    gradient = parallel.gatherCells(gradient, mesh.origMesh)
+    parallel.mpi.bcast(True)
+    gradient = adjoint(stackedAdjointFields)
     return gradient.reshape(N)
 
-vectors = [[] for sim in range(0, nEigen)]
+vectors = [None for sim in range(0, nEigen)]
 if parallel.rank == 0:
     A = linalg.LinearOperator((N,N), matvec=operator)
-    values, vectors = linalg.eigs(A, nEigen, tol=1e-3)
+    values, vectors = linalg.eigs(A, nEigen, tol=1e-6)
     vectors = vectors.reshape((totalCells, nDims, nEigen))
+    vectors = [vectors[:,:,i] for i in range(0, nEigen)]
+    parallel.mpi.bcast(False)
+else:
+    run = parallel.mpi.bcast(None)
+    while run:
+        adjoint(None)
+        run = parallel.mpi.bcast(None)
 
 for sim in range(0, nEigen):
-    stackedAdjointFields = parallel.scatterCells(vectors[:,:,sim], mesh.origMesh)
+    stackedAdjointFields = parallel.scatterCells(vectors[sim], mesh.origMesh)
     names = ['{0}a_{1}'.format(name, sim) for name in primal.names]
     writeAdjointFields(stackedAdjointFields, names, t)
