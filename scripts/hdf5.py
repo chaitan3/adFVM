236cefad182d3e4a3098a6bcdc68c77d21612d16
talnikar <talnikar@mit.edu>
2016-03-22 14:26:50 -0400

    conversion script

--- scripts/hdf5.py
+++ scripts/hdf5.py
@@ -1,2 +1,17 @@
-import config
-config.hdf5
+#!/usr/bin/python2
+
+from mesh import Mesh
+import parallel
+
+mesh = Mesh()
+
+import sys
+mesh.caseDir = sys.argv[1]
+mesh.case = mesh.caseDir + parallel.processorDirectory
+mesh.readFoam('constant') 
+mesh.writeHDF5()
+
+
+
+
+
