12bf0c0097898455f772b8b93c41cf25c230ae67
talnikar <talnikar@mit.edu>
2016-03-30 05:04:54 -0400

    fixed hdf5 sorting

--- scripts/hdf5serial.py
+++ scripts/hdf5serial.py
@@ -15,7 +15,9 @@ import BCs
 
 case = sys.argv[1]
 times = [float(x) for x in sys.argv[2:]]
-processorDirs = sorted(glob.glob(case + 'processor*'))
+processorDirs = glob.glob(case + 'processor*')
+ranks = [int(re.search('[0-9]+', proc).group(0)) for proc in processorDirs]
+processorDirs = [proc for (rank, proc) in sorted(zip(ranks, processorDirs))]
 
 meshFile = h5py.File(case + '/mesh.hdf5', 'w')
 
