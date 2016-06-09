c3ade38d411e8e18bdbbeda3d718137dcdd5e256
Chaitanya Talnikar <talnikar@mit.edu>
2016-06-09 10:39:00 -0400

    added field averaging utility

---
+++ scripts/field/avg_fields.py
@@ -0,0 +1,48 @@
+from field import Field, IOField
+from mesh import Mesh
+import sys
+import os
+import numpy as np
+import config
+
+def isfloat(s):
+    try:
+        f = float(s)
+        return True
+    except ValueError:
+        return False
+
+case, field = sys.argv[1:3]
+#times = sys.argv[3:]
+times = sorted([float(x) for x in os.listdir(case) if isfloat(x) ])
+mesh = Mesh.create(case)
+Field.setMesh(mesh)
+meshO = mesh.origMesh
+
+# time avg: no dt
+avg = 0.
+for time in times:
+    phi = IOField.read(field, mesh, time)
+    phi.partialComplete()
+    avg += phi.field
+avg /= len(times)
+
+# spanwise avg: structured
+nLayers = 200
+def average(start, end):
+    nCellsPerLayer = (end-start)/nLayers
+    spanAvg = avg[start:end].reshape((nLayers, nCellsPerLayer)).sum(axis=0)/nLayers
+    spanAvg = np.tile(spanAvg, (nLayers,1)).reshape(-1,1)
+    avg[start:end] = spanAvg
+
+average(0, meshO.nInternalCells)
+for patchID in mesh.localPatches:
+    patch = meshO.boundary[patchID]
+    if patch['type'] != 'cyclic':
+        cellStartFace = patch['startFace']-meshO.nInternalFaces + meshO.nInternalCells
+        cellEndFace = cellStartFace + patch['nFaces']
+        average(cellStartFace, cellEndFace)
+
+phi.name = field + '_avg'
+phi.field = avg
+phi.write(times[0])
