8614f343ed27248f70c7cc72cc3cc35575240e82
talnikar <talnikar@mit.edu>
2016-07-14 00:49:32 -0400

    voyager + bluewaters support

--- scripts/playground/test_multiple.py
+++ scripts/playground/test_multiple.py
@@ -1,16 +1,17 @@
-#!/usr/bin/python
-from mpi4py import MPI
+#!/usr/bin/python2
+import socket
 import subprocess
 
-print 
-nProcessors = 4
+nProcessors = 2
 program = './test_mpi4py.py'
 nCalls = 4
 
-print 'Host running on', MPI.Get_processor_name()
+print 'Host running on', socket.gethostname()
 a = []
 for i in range(0, nCalls):
-    p = subprocess.Popen(['srun', '-N', '1', '-n', str(nProcessors), '--resv-ports', program, str(i)])
+    p = subprocess.Popen(['aprun', '-n', str(nProcessors), 'python', program, str(i)]) 
+    #p = subprocess.Popen(['mpirun', '-n', str(nProcessors), program, str(i)])
+    #p = subprocess.Popen(['srun', '-N', '1', '-n', str(nProcessors), '--resv-ports', program, str(i)])
     a.append(p)
 for i in range(0, nCalls):
     p.wait()
