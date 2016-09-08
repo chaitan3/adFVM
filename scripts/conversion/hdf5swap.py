aa27d8d4ef4c46d5516361169a00cd8736bd5fff
Chaitanya Talnikar <talnikar@mit.edu>
2016-09-07 23:58:18 -0400

    hdf5swap works

--- scripts/conversion/hdf5swap.py
+++ scripts/conversion/hdf5swap.py
@@ -7,14 +7,19 @@ files = ['mesh.hdf5', '3.hdf5']
 
 for f in files:
     orig = h5py.File(f)
-    #swap = h5py.File('swapped/' + f)
+    swap = h5py.File('swapped/' + f, 'w')
 
-    def recurse(h, loc):
-        for key in h.keys()
-            val = h[key]
-            if isinstance(val, h5py.Dataset):
-                print 'group', key, loc
-                recurse(key, loc + '/' + key)
+    def recurse(h, h2, loc):
+        for key, val in h.items():
+            new_loc = loc + '/' + key
+            if not isinstance(val, h5py.Dataset):
+                print f, 'group', new_loc
+                h2_new = h2.create_group(new_loc)
+                recurse(val, h2_new, new_loc)
             else:
-                print 'val', key, val.dtype, loc
-    recurse(orig, '/')
+                print f, 'dataset', new_loc, val.dtype
+                val_new = val[:].copy().byteswap().newbyteorder()
+                h2.create_dataset(new_loc, data=val_new)
+    recurse(orig, swap, '')
+    orig.close()
+    swap.close()
