trying to fix adjoint scaling

adFVM/matop_petsc.py
@@ -1,3 +1,4 @@
+from . import config
 from .parallel import pprint
 from .compat import add_at
 
@@ -66,16 +67,16 @@ class Matrix(object):
         #pc.setFactorSolverPackage('mumps')
         #pc.setFactorSolverPackage('superlu_dist')
 
-        #ksp.setType('gmres')
-        ksp.setType('gcr')
+        ksp.setType('gmres')
+        #ksp.setType('gcr')
         #ksp.setType('bcgs')
         #ksp.setType('tfqmr')
         #ksp.getPC().setType('jacobi')
         #ksp.getPC().setType('asm')
         #ksp.getPC().setType('mg')
-        #ksp.getPC().setType('gamg')
+        ksp.getPC().setType('gamg')
         # which one is used?
-        ksp.getPC().setType('hypre')
+        #ksp.getPC().setType('hypre')
 
         x = self.A.createVecRight()
         X = []
@@ -112,7 +113,7 @@ def laplacian(phi, DT):
     jl, jh = A.getOwnershipRangeColumn()
     faceData = (mesh.areas*DT.field/mesh.deltas).flatten()
 
-    neighbourData = faceData[meshC.cellFaces]
+    neighbourData = faceData[mesh.cellFaces]
     neighbourData /= mesh.volumes
     row = np.arange(0, n, dtype=np.int32).reshape(-1,1)
     

apps/adjoint.py
@@ -150,7 +150,9 @@ class Adjoint(Solver):
                     #start3 = time.time()
 
                     stackedFields = np.concatenate([phi.field for phi in fields], axis=1)
-                    newStackedFields = (ddt(stackedFields, dt) - laplacian(stackedFields, weight)).solve()
+                    stackedPhi = Field('a', stackedFields, (5,))
+                    stackedPhi.old = stackedFields
+                    newStackedFields = (ddt(stackedPhi, dt) - laplacian(stackedPhi, weight)).solve()
                     fields[0][:nInternalCells] = newStackFields[:, [0]]
                     fields[1][:nInternalCells] = newStackFields[:, [1,2,3]]
                     fields[2][:nInternalCells] = newStackFields[:, [4]]