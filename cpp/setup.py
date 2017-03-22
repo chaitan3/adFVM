8af4745277700aa74531d219f6be40d6e93933be
Chaitanya Talnikar <talnikar@mit.edu>
2017-03-22 10:40:10 -0400

    fixed setup for mira

--- cpp/setup.py
+++ cpp/setup.py
@@ -7,24 +7,31 @@ from os.path import expanduser
 home = expanduser("~")
 
 os.environ['CC'] = 'ccache mpicc'
+os.environ['CXX'] = 'mpicc'
 #os.environ['CC'] = 'mpicc'
-os.environ['CXX'] = 'mpicxx'
-#os.environ['LDSHARED'] = 'mpicc'
+#os.environ['CXX'] = 'mpicc'
+#os.environ['CC'] = '/home/talnikar/local/bin/gcc'
+#os.environ['CXX'] = '/home/talnikar/local/bin/gcc'
+
+incdirs = [np.get_include(), home + '/sources/CoDiPack/include']
+libdirs = []
+#incdirs += ['/projects/LESOpt/talnikar/local/include']
+#libdirs += ['/projects/LESOpt/talnikar/local/lib/']
 
 for module, args in zip(['adFVMcpp', 'adFVMcpp_ad'], ['', '-DADIFF']):
-    compile_args = ['-std=c++11', '-O3', '-march=native']
+    compile_args = ['-std=c++11', '-O3']#, '-march=native']
+    #compile_args=['-std=c++11', '-O0', '-g']
     if len(args) > 0:
         compile_args += [args]
     mod = Extension(module,
                     sources = ['density.cpp', 'interface.cpp', 'interp.cpp', 'op.cpp', 'timestep.cpp', 'riemann.cpp', 'objective.cpp'],
                     extra_compile_args=compile_args,
-                    #extra_linker_args=['--whole-archive'],
-                    libraries=['AMPI'])
-                    #extra_compile_args=['-std=c++11', '-O0', '-g'])
+                    library_dirs=libdirs,
+                    libraries=['AMPI'],
+                    include_dirs=incdirs)
 
     setup (name = module,
            version = '0.0.1',
            description = 'This is a demo package',
-           ext_modules = [mod],
-           include_dirs=[np.get_include(), home + '/sources/CoDiPack/include', '/usr/local/include']
-           )
+           ext_modules = [mod])
+
