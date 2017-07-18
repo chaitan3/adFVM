65cb8b150d3399842238c647b36c6b171497dc59
Chaitanya Talnikar <talnikar@mit.edu>
2017-07-18 16:18:19 +0000

    vane optim fixes

--- cases/vane_optim/lorenz/replace.py
+++ cases/vane_optim/lorenz/replace.py
@@ -4,9 +4,10 @@ with open('state.pkl') as f:
     b = pickle.load(f)
 print b.keys(), [len(b[x]) for x in b.keys()]
 print b; exit(1)
-b['state'] = b['state'][:-1]
-b['points'] = b['points'][:-1]
+#b['state'] = b['state'][:-1]
+#b['points'] = b['points'][:-1]
 #b['state'] = ['ADJOINT' for i in range(0, 8)]
+#b['state'][-1] = 'MESH'
 #for i in range(0, len(b['evals'])):
 #    for j in range(6, 10):
 #        b['evals'][i][j] = b['evals'][i][j]*10
