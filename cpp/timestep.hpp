417471923c4aa8fcc325e363e87a450fd2554c00
Chaitanya Talnikar <talnikar@mit.edu>
2017-08-09 21:58:07 -0400

    delete cpp

--- cpp/timestep.hpp
+++
@@ -1,4 +0,0 @@
-#include "density.hpp"
-
-tuple<scalar, scalar> euler(RCF *rcf, const vec& rho, const mat& rhoU, const vec& rhoE, vec& rhoN, mat& rhoUN, vec& rhoEN, scalar t, scalar dt);
-tuple<scalar, scalar> SSPRK(RCF *rcf, const vec& rho, const mat& rhoU, const vec& rhoE, vec& rhoN, mat& rhoUN, vec& rhoEN, scalar t, scalar dt);
