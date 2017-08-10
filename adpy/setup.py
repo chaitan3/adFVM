aa8e5578fa8d79cafe6fe3d9934d1c9072bb1ee3
Chaitanya Talnikar <talnikar@mit.edu>
2017-08-09 21:58:42 -0400

    remove adpy

--- adpy/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-import numpy as np
-
-mod = Extension('function', 
-                sources=['util.c'], 
-                include_dirs=[np.get_include()],
-                extra_compile_args=['-g', '-O0'])
-setup(ext_modules=[mod])
-
