f3b9824624f23bb6e1dbb85d8ef94183349cada5
Chaitanya Talnikar <talnikar@mit.edu>
2017-08-17 21:30:33 +0000

    fixed this

--- cases/vane_optim/lorenz/replace.py
+++ cases/vane_optim/lorenz/replace.py
@@ -1,16 +1,24 @@
 import pickle
+import sys
+
 
 with open('state.pkl') as f:
     b = pickle.load(f)
-print b.keys(), [len(b[x]) for x in b.keys()]
-print b; exit(1)
+print [len(b[x]) for x in b.keys()]
+print [x[0] for x in b['evals']]
+if len(sys.argv) > 1:
+    status = sys.argv[1]
+    b['state'][-1] = status
+else:
+    print b; exit(1)
+
 #b['state'] = b['state'][:-1]
 #b['points'] = b['points'][:-1]
 #b['state'] = ['ADJOINT' for i in range(0, 8)]
-b['state'][-1] = 'PRIMADJ'
 #for i in range(0, len(b['evals'])):
 #    for j in range(6, 10):
 #        b['evals'][i][j] = b['evals'][i][j]*10
+
 with open('state.pkl', 'w') as f:
     pickle.dump(b, f)
 
