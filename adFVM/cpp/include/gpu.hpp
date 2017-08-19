17b18249d3acfb1aa3de0bee2397967ab47cf1a7
Chaitanya Talnikar <talnikar@mit.edu>
2017-08-18 22:14:01 -0400

    added missing files

---
+++ adFVM/cpp/include/gpu.hpp
@@ -0,0 +1,140 @@
+#ifndef GPU_HPP
+#define GPU_HPP
+
+#define GPU_THREADS_PER_BLOCK 256
+#define GPU_MAX_BLOCKS 65536
+//#define GPU_THREADS_PER_BLOCK 512
+//#define GPU_THREADS_PER_BLOCK 1024
+#define gpuErrorCheck(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      assert(!abort);
+      if (abort) exit(code);
+   }
+}
+
+template<typename dtype, integer shape1, integer shape2>
+__global__ void _extract1(const integer n, dtype* phi1, const dtype* phi2, const integer* indices) {
+    integer i = threadIdx.x + blockDim.x*blockIdx.x;
+    if (i < n) {
+        integer p = indices[i];
+        for (integer j = 0; j < shape1; j++) {
+            for (integer k = 0; k < shape2; k++) {
+                 atomicAdd(&phi1[p*shape1*shape2 + j*shape2 + k], phi2[i*shape1*shape2 + j*shape2 + k]);
+            }
+        }
+    }
+}
+
+template<typename dtype, integer shape1, integer shape2>
+__global__ void _extract2(const integer n, dtype* phi1, const dtype* phi2, const integer* indices) {
+    integer i = threadIdx.x + blockDim.x*blockIdx.x;
+    if (i < n) {
+        integer p = indices[i];
+        for (integer j = 0; j < shape1; j++) {
+            for (integer k = 0; k < shape2; k++) {
+                phi1[i*shape1*shape2 + j*shape2 + k] += phi2[p*shape1*shape2 + j*shape2 + k];
+            }
+        }
+    }
+}
+
+
+template <typename dtype, integer shape1=1, integer shape2=1, integer shape3=1>
+class gpuArrType: public arrType<dtype, shape1, shape2, shape3> {
+    public:
+    gpuArrType(const integer shape, bool zero=false) {
+        this -> init(shape);
+        gpuErrorCheck(cudaMalloc(&this->data, this->size*sizeof(dtype)));
+        this->inc_mem();
+        this->ownData = true;
+        if (zero) 
+            this -> zero();
+    }
+    gpuArrType(const integer shape, dtype* data) {
+        this -> init(shape);
+        this -> data = data;
+    }
+    void destroy() {
+        if (this->ownData && this->data != NULL) {
+            this->dec_mem();
+            gpuErrorCheck(cudaFree(this->data));
+            this -> data = NULL;
+        }
+    }
+    void zero() {
+        gpuErrorCheck(cudaMemset(this->data, 0, this->size*sizeof(dtype)));
+    }
+    void copy(integer index, dtype* sdata, integer n) {
+        gpuErrorCheck(cudaMemcpy(&(*this)(index), sdata, n*sizeof(dtype), cudaMemcpyDeviceToDevice));
+    }
+    void extract(const integer index, const integer* indices, const dtype* phiBuf, const integer n) {
+        integer blocks = n/GPU_THREADS_PER_BLOCK + 1;
+        integer threads = min(GPU_THREADS_PER_BLOCK, n);
+        _extract2<dtype, shape1, shape2><<<blocks, threads>>>(n, &(*this)(index), phiBuf, indices);
+        gpuErrorCheck(cudaPeekAtLastError());
+    }
+    void extract(const integer *indices, const dtype* phiBuf, const integer n) {
+        integer blocks = n/GPU_THREADS_PER_BLOCK + 1;
+        integer threads = min(GPU_THREADS_PER_BLOCK, n);
+        _extract1<dtype, shape1, shape2><<<blocks, threads>>>(n, this->data, phiBuf, indices);
+        gpuErrorCheck(cudaPeekAtLastError());
+        //gpuErrorCheck(cudaDeviceSynchronize());
+    }
+    
+    dtype* toHost() const {
+        dtype* hdata = new dtype[this->size];
+        gpuErrorCheck(cudaMemcpy(hdata, this->data, this->size*sizeof(dtype), cudaMemcpyDeviceToHost));
+        return hdata;
+    }
+    void toDevice(dtype* data) {
+        assert (this->data == NULL);
+        this->inc_mem();
+        gpuErrorCheck(cudaMalloc(&this->data, this->size*sizeof(dtype)));
+        gpuErrorCheck(cudaMemcpy(this->data, data, this->size*sizeof(dtype), cudaMemcpyHostToDevice));
+        this->ownData = true;
+    }
+    void info() const {
+        dtype minPhi, maxPhi;
+        minPhi = 1e30;
+        maxPhi = -1e30;
+        integer minLoc = -1, maxLoc = -1;
+        dtype* hdata = this->toHost();
+        for (integer i = 0; i < this->size; i++) {
+            if (hdata[i] < minPhi) {
+                minPhi = hdata[i];
+                minLoc = i;
+            }
+            if (hdata[i] > maxPhi) {
+                maxPhi = hdata[i];
+                maxLoc = i;
+            }
+
+        }
+        delete[] hdata;
+        cout << "phi min/max:" << minPhi << " " << maxPhi << endl;
+        //cout << "loc min/max:" << minLoc << " " << maxLoc << endl;
+    }
+    gpuArrType() : arrType<dtype, shape1, shape2, shape3>() {}
+    gpuArrType& operator=(gpuArrType&& that) {
+        assert(this != &that);
+        this->destroy();
+        //this->move(that);
+        this->init(that.shape);
+        this->data = that.data;
+        this->ownData = that.ownData;
+        that.ownData = false;
+        return *this;
+
+    }
+    ~gpuArrType() {
+        this->destroy();
+    };
+};
+
+#define extArrType gpuArrType
+
+#endif
---
+++ adFVM/gencode/graph.cpp
@@ -0,0 +1,140 @@
+#include "interface.hpp"
+#include "parallel.hpp"
+#include "mesh.hpp"
+
+struct memory mem = {0, 0};
+
+long long mil = 0;
+long long current_timestamp() {
+    struct timeval te; 
+    gettimeofday(&te, NULL); // get current time
+    long long milliseconds = te.tv_sec*1000LL + te.tv_usec/1000; // caculate milliseconds
+    //printf("milliseconds: %lld\n", milliseconds);
+    return milliseconds;
+}
+
+void Mesh::build() {}
+void Mesh::buildBeforeWrite() {}
+
+Mesh *meshp = NULL;
+#ifdef MATOP
+    #include "matop.hpp"
+    Matop *matop;
+#endif
+
+#define MODULE graph
+#ifdef PY3
+    #define initFunc GET_MODULE(PyInit_,MODULE)
+#else
+    #define initFunc GET_MODULE(init,MODULE)
+#endif
+#define modName VALUE(MODULE)
+
+PyObject* initialize(PyObject *self, PyObject *args) {
+
+    PyObject *meshObject = PyTuple_GetItem(args, 0);
+    Py_INCREF(meshObject);
+
+    meshp = new Mesh(meshObject);
+    meshp->init();
+    meshp->localRank = PyInt_AsLong(PyTuple_GetItem(args, 1));
+    parallel_init();
+
+    #ifdef GPU
+        int count;
+        gpuErrorCheck(cudaGetDeviceCount(&count));
+        printf("GPU devices: %d, rank: %d\n", count, meshp->rank);
+        cudaSetDevice(0);
+        //gpuErrorCheck(cudaSetDevice(meshp->localRank));
+    #endif
+
+    #ifdef MATOP
+        integer argc = 0;
+        PetscInitialize(&argc, NULL, NULL, NULL);
+        matop = new Matop();
+    #endif
+
+    return Py_None;
+}
+
+PyObject* viscositySolver(PyObject *self, PyObject *args) {
+
+    //cout << "forward 1" << endl;
+    PyObject *uObject, *DTObject;
+    scalar dt;
+    PyArg_ParseTuple(args, "OOd", &uObject, &DTObject, &dt);
+
+    arrType<scalar, 5> u;
+    vec DT;
+    getArray((PyArrayObject *)uObject, u);
+    getArray((PyArrayObject *)DTObject, DT);
+
+    const Mesh& mesh = *meshp;
+    arrType<scalar, 5> un(mesh.nInternalCells, true);
+    #ifdef MATOP
+        matop->heat_equation(u, DT, dt, un);
+    #endif
+    
+    return putArray(un);
+}
+
+
+
+PyMethodDef StaticMethods[] = {
+    {"initialize",  initialize, METH_VARARGS, "Execute a shell command."},
+    {"viscositySolver",  viscositySolver, METH_VARARGS, "Execute a shell command."},
+};
+extern PyMethodDef ExtraMethods[];
+static PyMethodDef* Methods;
+
+void interface_exit() {
+    parallel_exit();
+    delete meshp;
+    #ifdef MATOP
+        PetscFinalize();
+        delete matop;
+    #endif
+    free(Methods);
+}
+
+PyMODINIT_FUNC
+initFunc(void)
+{
+    PyObject *m;
+    int n = 0;
+    while(1) {
+        if (ExtraMethods[n].ml_name == NULL) 
+            break;
+        n++;
+    }
+    n++;
+    Methods = (PyMethodDef*) malloc(sizeof(PyMethodDef)*(2+n));
+    memcpy(Methods, StaticMethods, sizeof(PyMethodDef)*2);
+    memcpy(Methods + 2, ExtraMethods, sizeof(PyMethodDef)*n);
+
+    #ifdef PY3
+        static struct PyModuleDef moduledef = {
+            PyModuleDef_HEAD_INIT,  /* m_base */
+            modName,                 /* m_name */
+            NULL,                   /* m_doc */
+            -1,                     /* m_size */
+            Methods            /* m_methods */
+        };
+        m = PyModule_Create(&moduledef);
+    #else
+        m = Py_InitModule(modName, Methods);
+        if (m == NULL)
+            return;
+    #endif
+    import_array();
+    Py_AtExit(interface_exit);
+
+    //SpamError = PyErr_NewException("spam.error", NULL, NULL);
+    //Py_INCREF(SpamError);
+    //PyModule_AddObject(m, "error", SpamError);
+    #ifdef PY3
+        return m;
+    #endif
+}
+
+
