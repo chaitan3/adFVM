5d716df5c8b098ef137d0683c53fe8c2ab2d617c
Chaitanya Talnikar <talnikar@mit.edu>
2018-02-05 11:41:36 -0500

    fic

--- cases/vane_optim/lorenz/plot_hist.py
+++ cases/vane_optim/lorenz/plot_hist.py
@@ -16,8 +16,8 @@ plt.rcParams.update({'figure.figsize':(10, 6)})
 for beta in [0.0, 1.0, 2.0, 3.0, 10.0]:
 #for beta in [0.0]:
     #f = 'beta_{}/optim_nd2_lorenz.pkl'.format(beta)
-    #f = 'beta_{}/optim_nd2_rastigrin.pkl'.format(beta)
-    f = 'beta_{}/optim_nd2_rosenbrock5.pkl'.format(beta)
+    f = 'beta_{}/optim_nd2_rastigrin8.pkl'.format(beta)
+    #f = 'beta_{}/optim_nd2_rosenbrock5.pkl'.format(beta)
     try:
         a = pkl.load(open(f))
     except:
@@ -27,8 +27,8 @@ for beta in [0.0, 1.0, 2.0, 3.0, 10.0]:
     for b in a[0]:
     #for b in a[1]:
         #y.append([np.abs(x[1]) for x in b])
-        y.append([np.sqrt(((x[0]-np.array([1.,1]))**2).sum()) for x in b])
-        #y.append([np.sqrt(((x[0]-np.array([0.,0]))**2).sum()) for x in b])
+        #y.append([np.sqrt(((x[0]-np.array([1.,1]))**2).sum()) for x in b])
+        y.append([np.sqrt(((x[0]-np.array([0.,0]))**2).sum()) for x in b])
 
         #y.append([x[1]-0.5422 for x in b])
         #y.append([(np.sqrt((x[0]-np.array([26.7035,2.59748]))**2).sum()) for x in b])
@@ -40,7 +40,8 @@ for beta in [0.0, 1.0, 2.0, 3.0, 10.0]:
     #plt.hist(y[:,50], bins=np.arange(0.1, 0.2, 0.002))
     #plt.semilogy(y.mean(axis=0), label='beta = {}'.format(beta))
     #plt.plot(y.mean(axis=0), label=r'$\beta$ = {}'.format(beta))
-    plt.plot(np.median(y, axis=0), label=r'$\beta$ = {}'.format(beta))
+    #plt.plot(np.median(y, axis=0), label=r'$\beta$ = {}'.format(beta))
+    plt.semilogy(np.median(y, axis=0), label=r'$\beta$ = {}'.format(beta))
 plt.xlabel('Iteration number')
 plt.ylabel('Distance from minimum value')
 plt.legend()
