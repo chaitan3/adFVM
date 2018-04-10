d52d77adb4f0d7f61bba6600f64809a6b7a7bd0b
Chaitanya Talnikar <talnikar@mit.edu>
2018-04-10 17:39:07 -0400

    fix petsc symm

--- adFVM/cpp/matop_petsc.cpp
+++ adFVM/cpp/matop_petsc.cpp
@@ -38,192 +38,193 @@ Matop::~Matop () {
     PetscFinalize();
 }
 
-/*int Matop::heat_equation(vector<ext_vec*> w, vector<ext_vec*> u, const ext_vec& DTF, const ext_vec& dt_vec, vector<ext_vec*> un, bool density) {*/
-    //const Mesh& mesh = *meshp;
-    //Vec x, b;
-    //Mat A;
-    //long long start = current_timestamp();
-    ////if (mesh.rank == 0) cout << "here1" << endl;
-    ////MPI_Barrier(MPI_COMM_WORLD);
-    ////exit(1);
-
-    //PetscInt n = mesh.nInternalCells;
-    //PetscInt il, ih;
-    //PetscInt jl, jh;
-
-    //#ifdef GPU
-        //const scalar* faceData = DTF.toHost();
-        //const scalar* dt_data = dt_vec.toHost();
-        //const scalar dt = dt_data[0];
-        //#define VecCreateMPITypeWithArray VecCreateMPICUDAWithArray
-        //#define VecCreateTypeWithArray VecCreateSeqCUDAWithArray
-        //#define VecPlaceType VecCUDAPlaceArray
-        //#define VecResetType VecCUDAResetArray
-        //#define SparseType "aijcusparse"
-        ////#define SparseType "aijcusp"
-    //#else
-        //const scalar* faceData = DTF.data;
-        //const scalar dt = dt_vec(0);
-        //#define VecCreateMPITypeWithArray VecCreateMPIWithArray
-        //#define VecCreateTypeWithArray VecCreateSeqWithArray
-        //#define VecPlaceType VecPlaceArray
-        //#define VecResetType VecResetArray
-        //#define SparseType "aij"
-    //#endif
-
-    //// assembly time less than 10% on CPU
-    //CHKERRQ(MatCreate(PETSC_COMM_WORLD, &A));
-    //CHKERRQ(MatSetSizes(A, n, n, PETSC_DETERMINE, PETSC_DETERMINE));
-    //MatSetType(A, SparseType);
-    ////MatSetFromOptions(A);
-    //if (mesh.nProcs > 1) {
-        //CHKERRQ(MatMPIAIJSetPreallocation(A, 7, NULL, 6, NULL));
-    //} else {
-        //CHKERRQ(MatSeqAIJSetPreallocation(A, 7, NULL));
-    //}
-    ////MatSetOption(A, MAT_NEW_NONZERO_ALLOCATION_ERR, PETSC_FALSE);
-    //CHKERRQ(MatGetOwnershipRange(A, &il, &ih));
-    //CHKERRQ(MatGetOwnershipRangeColumn(A, &jl, &jh));
-    //long long start4 = current_timestamp();
-    //if (mesh.rank == 0) cout << "matop init: " << start4-start << endl;
-
-    //for (PetscInt j = il; j < ih; j++) {
-        //PetscInt index = j-il;
-        //scalar neighbourData[6];
-        //scalar cellData = 0;
-        //PetscInt cols[6];
-        //for (PetscInt k = 0; k < 6; k++) {
-            //PetscInt f = mesh.cellFaces(index, k);
-            ////neighbourData[k] = -faceData[f]/mesh.volumes(index);
-            //neighbourData[k] = -faceData[f]/mesh.volumes(index)*dt;
-            //cols[k] = mesh.cellNeighbours(index, k);
-            //if (cols[k] > -1) {
-                //cols[k] += jl;
-            //} 
-            //if ((cols[k] > -1) || (f >= mesh.nLocalFaces)) {
-                //cellData -= neighbourData[k];
-            //}
-            //assert(std::isfinite(faceData[f]));
-            //assert(std::isfinite(neighbourData[k]));
-        //}
-        //assert(std::isfinite(cellData));
-        //CHKERRQ(MatSetValues(A, 1, &j, 6, cols, neighbourData, INSERT_VALUES));
-        ////CHKERRQ(MatSetValue(A, j, index + jl, cellData + 1./dt, INSERT_VALUES));
-        //CHKERRQ(MatSetValue(A, j, index + jl, cellData + 1., INSERT_VALUES));
-    //}
-
-
-    ////const integer* ranges = new integer[mesh.nProcs+1];
-    //const PetscInt* ranges;
-    //CHKERRQ(MatGetOwnershipRangesColumn(A, &ranges));
-
-    //for (auto& patch: boundaryNeighbours) {
-        //auto& neighbourIndices = patch.second;
-        //integer startFace, nFaces;
-        //tie(startFace, nFaces) = mesh.boundaryFaces.at(patch.first);
-        //integer proc = boundaryProcs.at(patch.first);//stoi(patchInfo.at("neighbProcNo"));
-        //for (integer j = 0; j < nFaces; j++) {
-            //integer f = startFace + j;
-            //integer p = mesh.owner(f);
-            //integer index = il + p;
-            //integer neighbourIndex = ranges[proc] + neighbourIndices(j);
-            ////scalar data = -faceData[f]/mesh.volumes(p);
-            //scalar data = -faceData[f]/mesh.volumes(p)*dt;
-            //CHKERRQ(MatSetValue(A, index, neighbourIndex, data, INSERT_VALUES));
-        //}
-    //} 
-
-    //CHKERRQ(MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY)); CHKERRQ(MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY));
-    //long long start2 = current_timestamp();
-    //if (mesh.rank == 0) cout << "matop_assembly: " << start2-start << endl;
-        
-    
-    //KSP ksp;
-    //PC pc;
-    //CHKERRQ(KSPCreate(PETSC_COMM_WORLD, &(ksp)));
-    //CHKERRQ(KSPSetOperators(ksp, A, A));
-    ////BEST CPU
-    //CHKERRQ(KSPSetType(ksp, KSPGMRES));
-    ////CHKERRQ(KSPSetType(ksp, KSPGCR));
-    ////CHKERRQ(KSPSetType(ksp, KSPBCGS));
-    ////BEST GPU
-    ////CHKERRQ(KSPSetType(ksp, KSPTFQMR));
-    ////CHKERRQ(KSPSetType(ksp, KSPPREONLY));
-
-    ////double rtol, atol, dtol;
-    ////int maxit;
-    ////KSPGetTolerances(ksp, &rtol, &atol, &dtol, &maxit);
-    ////cout << rtol << " " << atol << " " << dtol << " " << maxit << endl;
-    ////KSPSetTolerances(ksp, 1e-4, PETSC_DEFAULT, PETSC_DEFAULT, PETSC_DEFAULT);
-    //KSPSetInitialGuessNonzero(ksp, PETSC_TRUE);
-
-    //CHKERRQ(KSPGetPC(ksp, &(pc)));
-    ////BEST GPU
-    ////CHKERRQ(PCSetType(pc, PCJACOBI));
-    ////CHKERRQ(PCSetType(pc, PCASM));
-    ////CHKERRQ(PCSetType(pc, PCMG));
-    ////CHKERRQ(PCSetType(pc, PCGAMG));
-    ////BEST CPU
-    //CHKERRQ(PCSetType(pc, PCHYPRE));
-
-    ////CHKERRQ(PCSetType(pc, PCLU));
-    ////CHKERRQ(PCFactorSetMatSolverPackage(pc,MATSOLVERSUPERLU_DIST));
-
-
-    ////KSPSetFromOptions(ksp);
-    //CHKERRQ(KSPSetUp(ksp));
-
-    ////scalar *data1 = new scalar[n];
-    ////VecPlaceArray(b, data1);
-    //if (mesh.nProcs > 1) {
-        //CHKERRQ(VecCreateMPITypeWithArray(PETSC_COMM_WORLD, 1, n, PETSC_DECIDE, NULL, &b));
-        //CHKERRQ(VecCreateMPITypeWithArray(PETSC_COMM_WORLD, 1, n, PETSC_DECIDE, NULL, &x));
-    //} else {
-        //CHKERRQ(VecCreateTypeWithArray(PETSC_COMM_WORLD, 1, n, NULL, &b));
-        //CHKERRQ(VecCreateTypeWithArray(PETSC_COMM_WORLD, 1, n, NULL, &x));
-        ////CHKERRQ(MatCreateVecs(A, &x, &b));
-    //}
-    //integer i = 0;
-    //if (!density) {
-        //un[i]->copy(0, u[i]->data, n);
-        //i = 1;
-    //}
-    //for (; i < nrhs; i++) {
-        //un[i]->copy(0, u[i]->data, n);
-        //CHKERRQ(VecPlaceType(b, u[i]->data));
-        //CHKERRQ(VecPlaceType(x, un[i]->data));
-        
-        //CHKERRQ(KSPSolve(ksp, b, x));
-        //KSPConvergedReason reason;
-        //KSPGetConvergedReason(ksp, &reason);
-        //PetscInt its;
-        //KSPGetIterationNumber(ksp, &its);
-	//if (mesh.rank == 0) {
-	    //PetscPrintf(PETSC_COMM_WORLD,"KSPConvergedReason: %D\n", reason);
-	    //PetscPrintf(PETSC_COMM_WORLD,"KSPIterations: %d\n", its);
-        //}
-        //if (reason < 0)  {
-            //return 1;
-        //}
-
-        //CHKERRQ(VecResetType(b));
-        //CHKERRQ(VecResetType(x));
-        //long long start3 = current_timestamp();
-        //if (mesh.rank == 0) cout << "ksp: " << start3-start << endl;
-    //}
-    ////VecResetArray(b);
-    ////delete[] data1;
-    //#ifdef GPU
-        //delete[] dt_data;
-        //delete[] faceData;
-    //#endif
-
-    //CHKERRQ(KSPDestroy(&ksp));
-    //CHKERRQ(MatDestroy(&A));
-    //CHKERRQ(VecDestroy(&b));
-    //CHKERRQ(VecDestroy(&x));
-    //return 0;
-/*}*/
+//int Matop::heat_equation(vector<ext_vec*> w, vector<ext_vec*> u, const ext_vec& DTF, const ext_vec& dt_vec, vector<ext_vec*> un, bool density) {
+//    const Mesh& mesh = *meshp;
+//    Vec x, b;
+//    Mat A;
+//    long long start = current_timestamp();
+//    //if (mesh.rank == 0) cout << "here1" << endl;
+//    //MPI_Barrier(MPI_COMM_WORLD);
+//    //exit(1);
+//
+//    PetscInt n = mesh.nInternalCells;
+//    PetscInt il, ih;
+//    PetscInt jl, jh;
+//
+//    #ifdef GPU
+//        const scalar* faceData = DTF.toHost();
+//        const scalar* dt_data = dt_vec.toHost();
+//        const scalar dt = dt_data[0];
+//        #define VecCreateMPITypeWithArray VecCreateMPICUDAWithArray
+//        #define VecCreateTypeWithArray VecCreateSeqCUDAWithArray
+//        #define VecPlaceType VecCUDAPlaceArray
+//        #define VecResetType VecCUDAResetArray
+//        #define SparseType "aijcusparse"
+//        //#define SparseType "aijcusp"
+//    #else
+//        const scalar* faceData = DTF.data;
+//        const scalar dt = dt_vec(0);
+//        #define VecCreateMPITypeWithArray VecCreateMPIWithArray
+//        #define VecCreateTypeWithArray VecCreateSeqWithArray
+//        #define VecPlaceType VecPlaceArray
+//        #define VecResetType VecResetArray
+//        #define SparseType "aij"
+//    #endif
+//
+//    // assembly time less than 10% on CPU
+//    CHKERRQ(MatCreate(PETSC_COMM_WORLD, &A));
+//    CHKERRQ(MatSetSizes(A, n, n, PETSC_DETERMINE, PETSC_DETERMINE));
+//    MatSetType(A, SparseType);
+//    //MatSetFromOptions(A);
+//    if (mesh.nProcs > 1) {
+//        CHKERRQ(MatMPIAIJSetPreallocation(A, 7, NULL, 6, NULL));
+//    } else {
+//        CHKERRQ(MatSeqAIJSetPreallocation(A, 7, NULL));
+//    }
+//    //MatSetOption(A, MAT_NEW_NONZERO_ALLOCATION_ERR, PETSC_FALSE);
+//    CHKERRQ(MatGetOwnershipRange(A, &il, &ih));
+//    CHKERRQ(MatGetOwnershipRangeColumn(A, &jl, &jh));
+//    long long start4 = current_timestamp();
+//    if (mesh.rank == 0) cout << "matop init: " << start4-start << endl;
+//
+//    for (PetscInt j = il; j < ih; j++) {
+//        PetscInt index = j-il;
+//        scalar neighbourData[6];
+//        scalar cellData = 0;
+//        PetscInt cols[6];
+//        for (PetscInt k = 0; k < 6; k++) {
+//            PetscInt f = mesh.cellFaces(index, k);
+//            //neighbourData[k] = -faceData[f]/mesh.volumes(index);
+//            neighbourData[k] = -faceData[f]/mesh.volumes(index)*dt;
+//            cols[k] = mesh.cellNeighbours(index, k);
+//            if (cols[k] > -1) {
+//                cols[k] += jl;
+//            } 
+//            if ((cols[k] > -1) || (f >= mesh.nLocalFaces)) {
+//                cellData -= neighbourData[k];
+//            }
+//            assert(std::isfinite(faceData[f]));
+//            assert(std::isfinite(neighbourData[k]));
+//        }
+//        assert(std::isfinite(cellData));
+//        CHKERRQ(MatSetValues(A, 1, &j, 6, cols, neighbourData, INSERT_VALUES));
+//        //CHKERRQ(MatSetValue(A, j, index + jl, cellData + 1./dt, INSERT_VALUES));
+//        CHKERRQ(MatSetValue(A, j, index + jl, cellData + 1., INSERT_VALUES));
+//    }
+//
+//
+//    //const integer* ranges = new integer[mesh.nProcs+1];
+//    const PetscInt* ranges;
+//    CHKERRQ(MatGetOwnershipRangesColumn(A, &ranges));
+//
+//    for (auto& patch: boundaryNeighbours) {
+//        auto& neighbourIndices = patch.second;
+//        integer startFace, nFaces;
+//        tie(startFace, nFaces) = mesh.boundaryFaces.at(patch.first);
+//        integer proc = boundaryProcs.at(patch.first);//stoi(patchInfo.at("neighbProcNo"));
+//        for (integer j = 0; j < nFaces; j++) {
+//            integer f = startFace + j;
+//            integer p = mesh.owner(f);
+//            integer index = il + p;
+//            integer neighbourIndex = ranges[proc] + neighbourIndices(j);
+//            //scalar data = -faceData[f]/mesh.volumes(p);
+//            scalar data = -faceData[f]/mesh.volumes(p)*dt;
+//            CHKERRQ(MatSetValue(A, index, neighbourIndex, data, INSERT_VALUES));
+//        }
+//    } 
+//
+//    CHKERRQ(MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY)); CHKERRQ(MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY));
+//    long long start2 = current_timestamp();
+//    if (mesh.rank == 0) cout << "matop_assembly: " << start2-start << endl;
+//        
+//    
+//    KSP ksp;
+//    PC pc;
+//    CHKERRQ(KSPCreate(PETSC_COMM_WORLD, &(ksp)));
+//    CHKERRQ(KSPSetOperators(ksp, A, A));
+//    //BEST CPU
+//    CHKERRQ(KSPSetType(ksp, KSPGMRES));
+//    //CHKERRQ(KSPSetType(ksp, KSPGCR));
+//    //CHKERRQ(KSPSetType(ksp, KSPBCGS));
+//    //BEST GPU
+//    //CHKERRQ(KSPSetType(ksp, KSPTFQMR));
+//    //CHKERRQ(KSPSetType(ksp, KSPPREONLY));
+//
+//    //double rtol, atol, dtol;
+//    //int maxit;
+//    //KSPGetTolerances(ksp, &rtol, &atol, &dtol, &maxit);
+//    //cout << rtol << " " << atol << " " << dtol << " " << maxit << endl;
+//    //KSPSetTolerances(ksp, 1e-4, PETSC_DEFAULT, PETSC_DEFAULT, PETSC_DEFAULT);
+//    KSPSetInitialGuessNonzero(ksp, PETSC_TRUE);
+//
+//    CHKERRQ(KSPGetPC(ksp, &(pc)));
+//    //BEST GPU
+//    //CHKERRQ(PCSetType(pc, PCJACOBI));
+//    //CHKERRQ(PCSetType(pc, PCASM));
+//    //CHKERRQ(PCSetType(pc, PCMG));
+//    //CHKERRQ(PCSetType(pc, PCGAMG));
+//    //BEST CPU
+//    CHKERRQ(PCSetType(pc, PCHYPRE));
+//
+//    //CHKERRQ(PCSetType(pc, PCLU));
+//    //CHKERRQ(PCFactorSetMatSolverPackage(pc,MATSOLVERSUPERLU_DIST));
+//
+//
+//    //KSPSetFromOptions(ksp);
+//    CHKERRQ(KSPSetUp(ksp));
+//
+//    //scalar *data1 = new scalar[n];
+//    //VecPlaceArray(b, data1);
+//    if (mesh.nProcs > 1) {
+//        CHKERRQ(VecCreateMPITypeWithArray(PETSC_COMM_WORLD, 1, n, PETSC_DECIDE, NULL, &b));
+//        CHKERRQ(VecCreateMPITypeWithArray(PETSC_COMM_WORLD, 1, n, PETSC_DECIDE, NULL, &x));
+//    } else {
+//        CHKERRQ(VecCreateTypeWithArray(PETSC_COMM_WORLD, 1, n, NULL, &b));
+//        CHKERRQ(VecCreateTypeWithArray(PETSC_COMM_WORLD, 1, n, NULL, &x));
+//        //CHKERRQ(MatCreateVecs(A, &x, &b));
+//    }
+//    integer i = 0;
+//    if (!density) {
+//        un[i]->copy(0, u[i]->data, n);
+//        i = 1;
+//    }
+//    for (; i < nrhs; i++) {
+//        un[i]->copy(0, u[i]->data, n);
+//        CHKERRQ(VecPlaceType(b, u[i]->data));
+//        CHKERRQ(VecPlaceType(x, un[i]->data));
+//        
+//        CHKERRQ(KSPSolve(ksp, b, x));
+//        KSPConvergedReason reason;
+//        KSPGetConvergedReason(ksp, &reason);
+//        PetscInt its;
+//        KSPGetIterationNumber(ksp, &its);
+//        if (mesh.rank == 0) {
+//            PetscPrintf(PETSC_COMM_WORLD,"KSPConvergedReason: %D\n", reason);
+//            PetscPrintf(PETSC_COMM_WORLD,"KSPIterations: %d\n", its);
+//        }
+//        if (reason < 0)  {
+//            return 1;
+//        }
+//
+//        CHKERRQ(VecResetType(b));
+//        CHKERRQ(VecResetType(x));
+//        long long start3 = current_timestamp();
+//        if (mesh.rank == 0) cout << "ksp: " << start3-start << endl;
+//    }
+//    //VecResetArray(b);
+//    //delete[] data1;
+//    #ifdef GPU
+//        delete[] dt_data;
+//        delete[] faceData;
+//    #endif
+//    printf("sum %lf\n", un[0]->sum());
+//
+//    CHKERRQ(KSPDestroy(&ksp));
+//    CHKERRQ(MatDestroy(&A));
+//    CHKERRQ(VecDestroy(&b));
+//    CHKERRQ(VecDestroy(&x));
+//    return 0;
+//}
 
 
 int Matop::heat_equation(vector<ext_vec*> w, vector<ext_vec*> u, const ext_vec& DTF, const ext_vec& dt_vec, vector<ext_vec*> un, bool density) {
@@ -296,21 +297,36 @@ int Matop::heat_equation(vector<ext_vec*> w, vector<ext_vec*> u, const ext_vec& 
         scalar H = g*p/(rho[index]*(g-1)) + q2/2;
 
         scalar A0[nrhs*nrhs];
-        A0[0*nrhs+0] = rho[index];
-        A0[0*nrhs+1] = rho[index]*u1;
-        A0[0*nrhs+2] = rho[index]*u2;
-        A0[0*nrhs+3] = rho[index]*u3;
-        A0[0*nrhs+4] = rhoE[index];
-        A0[1*nrhs+1] = rho[index]*u1*u1+p;
-        A0[1*nrhs+2] = rho[index]*u1*u2;
-        A0[1*nrhs+3] = rho[index]*u1*u3;
-        A0[1*nrhs+4] = rho[index]*H*u1;
-        A0[2*nrhs+2] = rho[index]*u2*u2+p;
-        A0[2*nrhs+3] = rho[index]*u2*u3;
-        A0[2*nrhs+4] = rho[index]*H*u2;
-        A0[3*nrhs+3] = rho[index]*u3*u3+p;
-        A0[3*nrhs+4] = rho[index]*H*u3;
-        A0[4*nrhs+4] = rho[index]*H*H-g*p*p/(rho[index]*(g-1));
+        //A0[0*nrhs+0] = rho[index];
+        //A0[0*nrhs+1] = rho[index]*u1;
+        //A0[0*nrhs+2] = rho[index]*u2;
+        //A0[0*nrhs+3] = rho[index]*u3;
+        //A0[0*nrhs+4] = rhoE[index];
+        //A0[1*nrhs+1] = rho[index]*u1*u1+p;
+        //A0[1*nrhs+2] = rho[index]*u1*u2;
+        //A0[1*nrhs+3] = rho[index]*u1*u3;
+        //A0[1*nrhs+4] = rho[index]*H*u1;
+        //A0[2*nrhs+2] = rho[index]*u2*u2+p;
+        //A0[2*nrhs+3] = rho[index]*u2*u3;
+        //A0[2*nrhs+4] = rho[index]*H*u2;
+        //A0[3*nrhs+3] = rho[index]*u3*u3+p;
+        //A0[3*nrhs+4] = rho[index]*H*u3;
+        //A0[4*nrhs+4] = rho[index]*H*H-g*p*p/(rho[index]*(g-1));
+        A0[0*nrhs+0] = 1;
+        A0[0*nrhs+1] = 0;
+        A0[0*nrhs+2] = 0;
+        A0[0*nrhs+3] = 0;
+        A0[0*nrhs+4] = 0;
+        A0[1*nrhs+1] = 1;
+        A0[1*nrhs+2] = 0;
+        A0[1*nrhs+3] = 0;
+        A0[1*nrhs+4] = 0;
+        A0[2*nrhs+2] = 1;
+        A0[2*nrhs+3] = 0;
+        A0[2*nrhs+4] = 0;
+        A0[3*nrhs+3] = 1;
+        A0[3*nrhs+4] = 0;
+        A0[4*nrhs+4] = 1;
         A0[1*nrhs+0] = A0[0*nrhs+1];
         A0[2*nrhs+1] = A0[1*nrhs+2];
         A0[2*nrhs+0] = A0[0*nrhs+2];
@@ -344,10 +360,13 @@ int Matop::heat_equation(vector<ext_vec*> w, vector<ext_vec*> u, const ext_vec& 
         for (PetscInt row = 0; row < nrhs; row++) {
             
             for (PetscInt col = 0; col < nrhs; col++) {
-                CHKERRQ(MatSetValue(A, row + j, col + j, A0[row*nrhs + col], INSERT_VALUES));
+                if (row == col) {
+                    CHKERRQ(MatSetValue(A, row + j, row + j, A0[row*nrhs + col] + cellData, INSERT_VALUES));
+                } else {
+                    CHKERRQ(MatSetValue(A, row + j, col + j, A0[row*nrhs + col], INSERT_VALUES));
+                }
                 bs[row] += A0[row*nrhs+col]*u[col]->data[index];
             }
-            CHKERRQ(MatSetValue(A, row + j, row + j, cellData, ADD_VALUES));
             PetscInt rowj = row + j;
             CHKERRQ(MatSetValues(A, 1, &rowj, 6, cols, neighbourData, INSERT_VALUES));
             for (PetscInt k = 0; k < 6; k++) {
@@ -446,6 +465,7 @@ int Matop::heat_equation(vector<ext_vec*> w, vector<ext_vec*> u, const ext_vec& 
             un[j]->data[i] = res[i*nrhs + j];
         }
     }
+    //printf("sum %lf\n", un[0]->sum());
 
     long long start3 = current_timestamp();
     if (mesh.rank == 0) cout << "ksp: " << start3-start << endl;
